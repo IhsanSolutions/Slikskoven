92c90376b7c6631694d9139d48d42c5458649690
ZoniD <deba0001@stud.ek.dk>
2026-05-07 11:00:37 +0200

    initial spring boot project

---
+++ src/main/java/dk/ek/slikskoven/SlikskovenApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.slikskoven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlikskovenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlikskovenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ek/slikskoven/SlikskovenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.slikskoven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlikskovenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
