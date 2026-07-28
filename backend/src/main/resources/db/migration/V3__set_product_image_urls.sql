45f28d73e0c98aa33e25d6c0cbd910eee2a9de9a
aljamour <aal.jamour9@gmail.com>
2026-07-28 17:45:17 +0000

    Optimize product images and configure production deployment

---
+++ backend/src/main/resources/db/migration/V3__set_product_image_urls.sql
@@ -0,0 +1,39 @@
+UPDATE product
+SET image_url = '/assets/products/gummi-bears.webp'
+WHERE name = 'Gummi Bears';
+
+UPDATE product
+SET image_url = '/assets/products/sour-straws.webp'
+WHERE name = 'Sour Straws';
+
+UPDATE product
+SET image_url = '/assets/products/chocolate-truffles.webp'
+WHERE name = 'Chocolate Truffles';
+
+UPDATE product
+SET image_url = '/assets/products/is.webp'
+WHERE name = 'Vanilla Ice Cream';
+
+UPDATE product
+SET image_url = '/assets/products/jordbaer-lakrids.webp'
+WHERE name = 'Strawberry Licorice';
+
+UPDATE product
+SET image_url = '/assets/products/espresso-is.webp'
+WHERE name = 'Espresso Ice Cream';
+
+UPDATE product
+SET image_url = '/assets/products/blue-raspberry-lollipops.webp'
+WHERE name = 'Blue Raspberry Lollipops';
+
+UPDATE product
+SET image_url = '/assets/products/softice.webp'
+WHERE name = 'Soft Ice Vanilla';
+
+UPDATE product
+SET image_url = '/assets/products/mixed-berries-slush.webp'
+WHERE name = 'Mixed Berries Slush';
+
+UPDATE product
+SET image_url = '/assets/products/salted-caramel-toffee.webp'
+WHERE name = 'Salted Caramel Toffee';
